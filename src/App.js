47a91287a6f5e72eb926a52aabadcff4adddd520
JaswanthReddyPadala <jaswanthreddypadala@gmail.com>
2021-08-28 14:47:22 +0530

    Completed for Desktop version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MoneyManager from './components/MoneyManager/MoneyManager.js'
+
+function App() {
+  return (
+    <div className='App'>
+     <MoneyManager />
+    </div>
+  );
+}
+
+export default App;
